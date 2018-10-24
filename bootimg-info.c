6f75bad1bb144512c979551c5c0efbab1addac18
osm0sis <osm0sis@outlook.com>
2018-10-24 01:46:56 -0300

    update for boot_img_hdr_v1 - backport bootimg.h changes to C from now-C++ https://github.com/aosp-mirror/platform_system_core/commit/6bacea2bec4fe09e33fb3f51bb887a862ddf2350 - detect dt_size vs. header_version and show new hdr_v1 fields - fix crazy inherited whitespace (finally) - fix pseudocolumns properly

--- bootimg-info.c
+++ bootimg-info.c
@@ -32,24 +32,24 @@ int main(int argc, char** argv)
     char tmp[PATH_MAX];
     char* filename = NULL;
     int base = 0;
-    
+
     argc--;
     if (argc > 0) {
         char *val = argv[1];
         filename = val;
     }
-    
+
     if (filename == NULL) {
         return usage();
     }
-    
+
     FILE* f = fopen(filename, "rb");
     if (!f) {
         printf("bootimg-info: File not found!\n");
         return 0;
     }
-    
-    boot_img_hdr header;
+
+    boot_img_hdr_v1 header;
     int i;
     int seeklimit = 65536;
     for (i = 0; i <= seeklimit; i++) {
@@ -70,55 +70,67 @@ int main(int argc, char** argv)
         int os_version,os_patch_level;
         os_version = header.os_version >> 11;
         os_patch_level = header.os_version&0x7ff;
-        
+
         a = (os_version >> 14)&0x7f;
         b = (os_version >> 7)&0x7f;
         c = os_version&0x7f;
-        
+
         y = (os_patch_level >> 4) + 2000;
         m = os_patch_level&0xf;
     }
-    
+    int hdr_ver_max = 4;
+
     printf(" Android Boot Image Info Utility\n\n");
-    
+
     printf(" Printing information for \"%s\"\n\n", filename);
-    
+
     printf(" Header:\n");
-    printf("  magic            : ANDROID!\n");
-    printf("  kernel_size      : %d  \t  (%08x)\n", header.kernel_size, header.kernel_size);
-    printf("  kernel_addr      : 0x%08x\n\n", header.kernel_addr);
-    
-    printf("  ramdisk_size     : %d  \t  (%08x)\n", header.ramdisk_size, header.ramdisk_size);
-    printf("  ramdisk_addr     : 0x%08x\n", header.ramdisk_addr);
-    printf("  second_size      : %d  \t  (%08x)\n", header.second_size, header.second_size);
-    printf("  second_addr      : 0x%08x\n\n", header.second_addr);
-    
-    printf("  tags_addr        : 0x%08x\n", header.tags_addr);
-    printf("  page_size        : %d  \t  (%08x)\n", header.page_size, header.page_size);
-    printf("  dt_size          : %d  \t  (%08x)\n", header.dt_size, header.dt_size);
+    printf("  magic                 : ANDROID!\n");
+    printf("  kernel_size           : %-10d  (%08x)\n", header.kernel_size, header.kernel_size);
+    printf("  kernel_addr           : 0x%08x\n\n", header.kernel_addr);
+
+    printf("  ramdisk_size          : %-10d  (%08x)\n", header.ramdisk_size, header.ramdisk_size);
+    printf("  ramdisk_addr          : 0x%08x\n", header.ramdisk_addr);
+    printf("  second_size           : %-10d  (%08x)\n", header.second_size, header.second_size);
+    printf("  second_addr           : 0x%08x\n\n", header.second_addr);
+
+    printf("  tags_addr             : 0x%08x\n", header.tags_addr);
+    printf("  page_size             : %-10d  (%08x)\n", header.page_size, header.page_size);
+    if (header.dt_size > hdr_ver_max) {
+        printf("  dt_size               : %-10d  (%08x)\n", header.dt_size, header.dt_size);
+    } else {
+        printf("  header_version        : %-10d  (%08x)\n", header.header_version, header.header_version);
+    }
     if ((a < 128) && (b < 128) && (c < 128) && (y >= 2000) && (y < 2128) && (m > 0) && (m <= 12)) {
-        printf("  os_version       : %d.%d.%d\n", a, b, c);
-        printf("  os_patch_level   : %d-%02d\n\n", y, m);
-    } else
-        printf("  unused           : %d  \t  (%08x)\n\n", header.os_version, header.os_version);
-    
-    printf("  name             : %s\n", header.name);
-    printf("  cmdline          : %.*s\n\n", BOOT_ARGS_SIZE, header.cmdline);
-    
-    printf("  id               : "); print_hash(header.id); printf("\n\n");
-    
-    printf("  extra_cmdline    : %.*s\n\n", BOOT_EXTRA_ARGS_SIZE, header.extra_cmdline);
-    
+        printf("  os_version            : %d.%d.%d\n", a, b, c);
+        printf("  os_patch_level        : %d-%02d\n\n", y, m);
+    } else {
+        printf("  unused                : %-10d  (%08x)\n\n", header.os_version, header.os_version);
+    }
+
+    printf("  name                  : %s\n", header.name);
+    printf("  cmdline               : %.*s\n\n", BOOT_ARGS_SIZE, header.cmdline);
+
+    printf("  id                    : "); print_hash(header.id); printf("\n\n");
+
+    printf("  extra_cmdline         : %.*s\n\n", BOOT_EXTRA_ARGS_SIZE, header.extra_cmdline);
+
+    if (header.header_version > 0 && header.header_version <= hdr_ver_max) {
+        printf("  recovery_dtbo_size    : %-10d  (%08x)\n", header.recovery_dtbo_size, header.recovery_dtbo_size);
+        printf("  recovery_dtbo_offset  : 0x%08x\n", header.recovery_dtbo_offset);
+        printf("  header_size           : %-10d  (%08x)\n\n", header.header_size, header.header_size);
+    }
+
     printf(" Other:\n");
-    printf("  magic offset     : 0x%08x\n", i);
-    printf("  base address     : 0x%08x\n\n", base);
-    
-    printf("  kernel offset    : 0x%08x\n", header.kernel_addr - base);
-    printf("  ramdisk offset   : 0x%08x\n", header.ramdisk_addr - base);
-    printf("  second offset    : 0x%08x\n", header.second_addr - base);
-    printf("  tags offset      : 0x%08x\n", header.tags_addr - base);
-    
+    printf("  magic offset          : 0x%08x\n", i);
+    printf("  base address          : 0x%08x\n\n", base);
+
+    printf("  kernel offset         : 0x%08x\n", header.kernel_addr - base);
+    printf("  ramdisk offset        : 0x%08x\n", header.ramdisk_addr - base);
+    printf("  second offset         : 0x%08x\n", header.second_addr - base);
+    printf("  tags offset           : 0x%08x\n", header.tags_addr - base);
+
     fclose(f);
-    
+
     return 0;
 }
--- include/bootimg.h
+++ include/bootimg.h
@@ -2,16 +2,16 @@
 **
 ** Copyright 2007, The Android Open Source Project
 **
-** Licensed under the Apache License, Version 2.0 (the "License"); 
-** you may not use this file except in compliance with the License. 
-** You may obtain a copy of the License at 
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
 **
-**     http://www.apache.org/licenses/LICENSE-2.0 
+**     http://www.apache.org/licenses/LICENSE-2.0
 **
-** Unless required by applicable law or agreed to in writing, software 
-** distributed under the License is distributed on an "AS IS" BASIS, 
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-** See the License for the specific language governing permissions and 
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
 ** limitations under the License.
 */
 
@@ -20,30 +20,39 @@
 #ifndef _BOOT_IMAGE_H_
 #define _BOOT_IMAGE_H_
 
-typedef struct boot_img_hdr boot_img_hdr;
-
 #define BOOT_MAGIC "ANDROID!"
 #define BOOT_MAGIC_SIZE 8
 #define BOOT_NAME_SIZE 16
 #define BOOT_ARGS_SIZE 512
 #define BOOT_EXTRA_ARGS_SIZE 1024
 
-struct boot_img_hdr
-{
+#define BOOT_HEADER_VERSION_ZERO 0
+/*
+ *  Bootloader expects the structure of boot_img_hdr with header version
+ *  BOOT_HEADER_VERSION_ZERO to be as follows:
+ */
+struct boot_img_hdr_v0 {
     uint8_t magic[BOOT_MAGIC_SIZE];
 
-    uint32_t kernel_size;  /* size in bytes */
-    uint32_t kernel_addr;  /* physical load addr */
+    uint32_t kernel_size; /* size in bytes */
+    uint32_t kernel_addr; /* physical load addr */
 
     uint32_t ramdisk_size; /* size in bytes */
     uint32_t ramdisk_addr; /* physical load addr */
 
-    uint32_t second_size;  /* size in bytes */
-    uint32_t second_addr;  /* physical load addr */
+    uint32_t second_size; /* size in bytes */
+    uint32_t second_addr; /* physical load addr */
 
-    uint32_t tags_addr;    /* physical addr for kernel tags */
-    uint32_t page_size;    /* flash page size we assume */
-    uint32_t dt_size;      /* device tree in bytes */
+    uint32_t tags_addr; /* physical addr for kernel tags */
+    uint32_t page_size; /* flash page size we assume */
+    /*
+     * version for the boot image header.
+     * alternately this is used as dt_size on some hardware.
+     */
+    union {
+        uint32_t header_version;
+        uint32_t dt_size; /* device tree in bytes */
+    };
 
     /* operating system version and security patch level; for
      * version "A.B.C" and patch level "Y-M-D":
@@ -64,34 +73,131 @@ struct boot_img_hdr
 } __attribute__((packed));
 
 /*
-** +-----------------+ 
-** | boot header     | 1 page
-** +-----------------+
-** | kernel          | n pages  
-** +-----------------+
-** | ramdisk         | m pages  
-** +-----------------+
-** | second stage    | o pages
-** +-----------------+
-** | device tree     | p pages
-** +-----------------+
-**
-** n = (kernel_size + page_size - 1) / page_size
-** m = (ramdisk_size + page_size - 1) / page_size
-** o = (second_size + page_size - 1) / page_size
-** p = (dt_size + page_size - 1) / page_size
-**
-** 0. all entities are page_size aligned in flash
-** 1. kernel and ramdisk are required (size != 0)
-** 2. second is optional (second_size == 0 -> no second)
-** 3. load each element (kernel, ramdisk, second) at
-**    the specified physical address (kernel_addr, etc)
-** 4. prepare tags at tag_addr.  kernel_args[] is
-**    appended to the kernel commandline in the tags.
-** 5. r0 = 0, r1 = MACHINE_TYPE, r2 = tags_addr
-** 6. if second_size != 0: jump to second_addr
-**    else: jump to kernel_addr
-*/
+ * It is expected that callers would explicitly specify which version of the
+ * boot image header they need to use.
+ */
+typedef struct boot_img_hdr_v0 boot_img_hdr;
+typedef struct boot_img_hdr_v0 boot_img_hdr_v0;
+typedef struct boot_img_hdr_v1 boot_img_hdr_v1;
+
+/* When a boot header is of version BOOT_HEADER_VERSION_ZERO, the structure of boot image is as
+ * follows:
+ *
+ * +-----------------+
+ * | boot header     | 1 page
+ * +-----------------+
+ * | kernel          | n pages
+ * +-----------------+
+ * | ramdisk         | m pages
+ * +-----------------+
+ * | second stage    | o pages
+ * +-----------------+
+ *
+ * and on some hardware:
+ * +-----------------+
+ * | device tree     | p pages
+ * +-----------------+
+ *
+ * n = (kernel_size + page_size - 1) / page_size
+ * m = (ramdisk_size + page_size - 1) / page_size
+ * o = (second_size + page_size - 1) / page_size
+ *
+ * and on some hardware:
+ * p = (dt_size + page_size -1) / page_size
+ *
+ * 0. all entities are page_size aligned in flash
+ * 1. kernel and ramdisk are required (size != 0)
+ * 2. second is optional (second_size == 0 -> no second)
+ * 3. load each element (kernel, ramdisk, second) at
+ *    the specified physical address (kernel_addr, etc)
+ * 4. prepare tags at tag_addr.  kernel_args[] is
+ *    appended to the kernel commandline in the tags.
+ * 5. r0 = 0, r1 = MACHINE_TYPE, r2 = tags_addr
+ * 6. if second_size != 0: jump to second_addr
+ *    else: jump to kernel_addr
+ */
+
+#define BOOT_HEADER_VERSION_ONE 1
+
+struct boot_img_hdr_v1 {
+    uint8_t magic[BOOT_MAGIC_SIZE];
+
+    uint32_t kernel_size; /* size in bytes */
+    uint32_t kernel_addr; /* physical load addr */
+
+    uint32_t ramdisk_size; /* size in bytes */
+    uint32_t ramdisk_addr; /* physical load addr */
+
+    uint32_t second_size; /* size in bytes */
+    uint32_t second_addr; /* physical load addr */
+
+    uint32_t tags_addr; /* physical addr for kernel tags */
+    uint32_t page_size; /* flash page size we assume */
+    /*
+     * version for the boot image header.
+     * alternately this is used as dt_size on some hardware.
+     */
+    union {
+        uint32_t header_version;
+        uint32_t dt_size; /* device tree in bytes */
+    };
+
+    /* operating system version and security patch level; for
+     * version "A.B.C" and patch level "Y-M-D":
+     * ver = A << 14 | B << 7 | C         (7 bits for each of A, B, C)
+     * lvl = ((Y - 2000) & 127) << 4 | M  (7 bits for Y, 4 bits for M)
+     * os_version = ver << 11 | lvl */
+    uint32_t os_version;
+
+    uint8_t name[BOOT_NAME_SIZE]; /* asciiz product name */
+
+    uint8_t cmdline[BOOT_ARGS_SIZE];
+
+    uint32_t id[8]; /* timestamp / checksum / sha1 / etc */
+
+    /* Supplemental command line data; kept here to maintain
+     * binary compatibility with older versions of mkbootimg */
+    uint8_t extra_cmdline[BOOT_EXTRA_ARGS_SIZE];
+
+    uint32_t recovery_dtbo_size;   /* size in bytes for recovery DTBO image */
+    uint64_t recovery_dtbo_offset; /* offset to recovery dtbo in boot image */
+    uint32_t header_size;
+} __attribute__((packed));
+
+/* When the boot image header has a version of BOOT_HEADER_VERSION_ONE, the structure of the boot
+ * image is as follows:
+ *
+ * +-----------------+
+ * | boot header     | 1 page
+ * +-----------------+
+ * | kernel          | n pages
+ * +-----------------+
+ * | ramdisk         | m pages
+ * +-----------------+
+ * | second stage    | o pages
+ * +-----------------+
+ * | recovery dtbo   | p pages
+ * +-----------------+
+ * n = (kernel_size + page_size - 1) / page_size
+ * m = (ramdisk_size + page_size - 1) / page_size
+ * o = (second_size + page_size - 1) / page_size
+ * p = (recovery_dtbo_size + page_size - 1) / page_size
+ *
+ * 0. all entities are page_size aligned in flash
+ * 1. kernel and ramdisk are required (size != 0)
+ * 2. recovery_dtbo is required for recovery.img in non-A/B devices(recovery_dtbo_size != 0)
+ * 3. second is optional (second_size == 0 -> no second)
+ * 4. load each element (kernel, ramdisk, second) at
+ *    the specified physical address (kernel_addr, etc)
+ * 5. If booting to recovery mode in a non-A/B device, extract recovery dtbo and
+ *    apply the correct set of overlays on the base device tree depending on the
+ *    hardware/product revision.
+ * 6. prepare tags at tag_addr.  kernel_args[] is
+ *    appended to the kernel commandline in the tags.
+ * 7. r0 = 0, r1 = MACHINE_TYPE, r2 = tags_addr
+ * 8. if second_size != 0: jump to second_addr
+ *    else: jump to kernel_addr
+ */
 
 #if 0
 typedef struct ptentry ptentry;
