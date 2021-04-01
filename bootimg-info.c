4d38488e037c0c6d5b99adf8cd593b640a0e636f
osm0sis <osm0sis@outlook.com>
2021-04-01 03:51:41 -0300

    update for boot_img_hdr_v3 and vendor_boot_img_hdr_v3
    - backport changes from upstream AOSP where bootimg.h has been converted from C to C++ format: https://android.googlesource.com/platform/system/tools/mkbootimg/+log/a0eaee9cbff3c2e3b67c03974a2da18df391ecee/
    - boot_img_hdr_v3 is no longer backwards compatible so detect and switch headers to handle
    - detect new VNDRBOOT magic and switch headers to handle

--- bootimg-info.c
+++ bootimg-info.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <inttypes.h>
 
@@ -16,121 +17,206 @@ static char print_hash(boot_img_hdr_v2 *hdr)
     uint8_t id[SHA256_DIGEST_SIZE];
     memcpy(&id, hdr->id, sizeof(id));
     int i;
-    for (i = 0; i < SHA256_DIGEST_SIZE; ++i)
+    for (i = 0; i < SHA256_DIGEST_SIZE; ++i) {
         printf("%02hhx", id[i]);
+    }
     return 0;
 }
 
 int main(int argc, char** argv)
 {
-    char tmp[BOOT_MAGIC_SIZE];
     char* filename = NULL;
-    int base = 0;
-
     argc--;
     if (argc > 0) {
         char *val = argv[1];
         filename = val;
     }
-
     if (filename == NULL) {
         return usage();
     }
-
     FILE* f = fopen(filename, "rb");
     if (!f) {
         printf("bootimg-info: File not found!\n");
         return 0;
     }
 
-    boot_img_hdr_v2 header;
+    char tmp[BOOT_MAGIC_SIZE];
+    bool vndrboot = false;
+
     int i;
-    int seeklimit = 65536;
+    int seeklimit = 65536; // arbitrary byte limit to search in input file for boot image magic
     for (i = 0; i <= seeklimit; i++) {
         fseek(f, i, SEEK_SET);
         if(fread(tmp, BOOT_MAGIC_SIZE, 1, f)){};
-        if (memcmp(tmp, BOOT_MAGIC, BOOT_MAGIC_SIZE) == 0)
+        if (memcmp(tmp, BOOT_MAGIC, BOOT_MAGIC_SIZE) == 0) {
             break;
+        }
+        if (memcmp(tmp, VENDOR_BOOT_MAGIC, VENDOR_BOOT_MAGIC_SIZE) == 0) {
+            vndrboot = true;
+            break;
+        }
     }
     if (i > seeklimit) {
-        printf("bootimg-info: Android boot magic not found!\n");
+        printf("bootimg-info: No boot image magic found!\n");
         return 1;
     }
-    fseek(f, i, SEEK_SET);
-    if(fread(&header, sizeof(header), 1, f)){};
-    base = header.kernel_addr - 0x00008000;
-    int a=0, b=0, c=0, y=0, m=0;
-    if (header.os_version != 0) {
-        int os_version,os_patch_level;
-        os_version = header.os_version >> 11;
-        os_patch_level = header.os_version&0x7ff;
-
-        a = (os_version >> 14)&0x7f;
-        b = (os_version >> 7)&0x7f;
-        c = os_version&0x7f;
-
-        y = (os_patch_level >> 4) + 2000;
-        m = os_patch_level&0xf;
-    }
-    int hdr_ver_max = 4;
 
     printf(" Android Boot Image Info Utility\n\n");
 
     printf(" Printing information for \"%s\"\n\n", filename);
 
     printf(" Header:\n");
-    printf("  magic                 : ANDROID!\n");
-    printf("  kernel_size           : %-10d  (%08x)\n", header.kernel_size, header.kernel_size);
-    printf("  kernel_addr           : 0x%08x\n\n", header.kernel_addr);
 
-    printf("  ramdisk_size          : %-10d  (%08x)\n", header.ramdisk_size, header.ramdisk_size);
-    printf("  ramdisk_addr          : 0x%08x\n", header.ramdisk_addr);
-    printf("  second_size           : %-10d  (%08x)\n", header.second_size, header.second_size);
-    printf("  second_addr           : 0x%08x\n\n", header.second_addr);
+    boot_img_hdr_v3 header;
+    fseek(f, i, SEEK_SET);
+    if(fread(&header, sizeof(header), 1, f)){};
 
-    printf("  tags_addr             : 0x%08x\n", header.tags_addr);
-    printf("  page_size             : %-10d  (%08x)\n", header.page_size, header.page_size);
-    if (header.dt_size > hdr_ver_max) {
-        printf("  dt_size               : %-10d  (%08x)\n", header.dt_size, header.dt_size);
+    int hdr_ver_max = 8; // arbitrary maximum header version value; when greater assume the field is appended dt size
+
+    int base = 0;
+
+    if (vndrboot == false) {
+        if ((header.header_version < 3) || (header.header_version > hdr_ver_max)) {
+            // boot_img_hdr_v2 in the modified header supports all boot_img_hdr versions and variants below 3
+            fseek(f, i, SEEK_SET);
+            boot_img_hdr_v2 header;
+            if(fread(&header, sizeof(header), 1, f)){};
+
+            base = header.kernel_addr - 0x00008000;
+
+            int a = 0, b = 0, c = 0, y = 0, m = 0;
+            if (header.os_version != 0) {
+                int os_version,os_patch_level;
+                os_version = header.os_version >> 11;
+                os_patch_level = header.os_version&0x7ff;
+
+                a = (os_version >> 14)&0x7f;
+                b = (os_version >> 7)&0x7f;
+                c = os_version&0x7f;
+
+                y = (os_patch_level >> 4) + 2000;
+                m = os_patch_level&0xf;
+            }
+
+            printf("  magic                 : ANDROID!\n");
+            printf("  kernel_size           : %-10d  (%08x)\n", header.kernel_size, header.kernel_size);
+            printf("  kernel_addr           : 0x%08x\n\n", header.kernel_addr);
+
+            printf("  ramdisk_size          : %-10d  (%08x)\n", header.ramdisk_size, header.ramdisk_size);
+            printf("  ramdisk_addr          : 0x%08x\n", header.ramdisk_addr);
+            printf("  second_size           : %-10d  (%08x)\n", header.second_size, header.second_size);
+            printf("  second_addr           : 0x%08x\n\n", header.second_addr);
+
+            printf("  tags_addr             : 0x%08x\n", header.tags_addr);
+            printf("  page_size             : %-10d  (%08x)\n", header.page_size, header.page_size);
+            if (header.dt_size > hdr_ver_max) {
+                printf("  dt_size               : %-10d  (%08x)\n", header.dt_size, header.dt_size);
+            } else {
+                printf("  header_version        : %-10d  (%08x)\n", header.header_version, header.header_version);
+            }
+            if ((a < 128) && (b < 128) && (c < 128) && (y >= 2000) && (y < 2128) && (m > 0) && (m <= 12)) {
+                printf("  os_version            : %d.%d.%d\n", a, b, c);
+                printf("  os_patch_level        : %d-%02d\n\n", y, m);
+            } else {
+                printf("  unused                : %-10d  (%08x)\n\n", header.os_version, header.os_version);
+            }
+
+            printf("  name                  : %s\n\n", header.name);
+
+            printf("  cmdline               : %.*s\n\n", BOOT_ARGS_SIZE, header.cmdline);
+
+            printf("  id                    : "); print_hash(&header); printf("\n\n");
+
+            printf("  extra_cmdline         : %.*s\n\n", BOOT_EXTRA_ARGS_SIZE, header.extra_cmdline);
+
+            if (header.header_version <= hdr_ver_max) {
+                if (header.header_version > 0) {
+                    printf("  recovery_dtbo_size    : %-10d  (%08x)\n", header.recovery_dtbo_size, header.recovery_dtbo_size);
+                    printf("  recovery_dtbo_offset  : %-10"PRId64"  (%016"PRIx64")\n", header.recovery_dtbo_offset, header.recovery_dtbo_offset);
+                    printf("  header_size           : %-10d  (%08x)\n\n", header.header_size, header.header_size);
+                }
+                if (header.header_version > 1) {
+                    printf("  dtb_size              : %-10d  (%08x)\n", header.dtb_size, header.dtb_size);
+                    printf("  dtb_addr              : 0x%08"PRIx64"  (%016"PRIx64")\n\n", header.dtb_addr, header.dtb_addr);
+                }
+            }
+
+            printf(" Other:\n");
+            printf("  magic offset          : 0x%08x\n", i);
+            printf("  base address          : 0x%08x\n\n", base);
+
+            printf("  kernel offset         : 0x%08x\n", header.kernel_addr - base);
+            printf("  ramdisk offset        : 0x%08x\n", header.ramdisk_addr - base);
+            printf("  second offset         : 0x%08x\n", header.second_addr - base);
+            printf("  tags offset           : 0x%08x\n", header.tags_addr - base);
+            if (header.header_version <= hdr_ver_max && header.header_version > 1) {
+                printf("  dtb offset            : 0x%08"PRIx64"\n", header.dtb_addr - base);
+            }
+        } else {
+            // boot_img_hdr_v3 and above are no longer backwards compatible
+            int a = 0, b = 0, c = 0, y = 0, m = 0;
+            int os_version,os_patch_level;
+            os_version = header.os_version >> 11;
+            os_patch_level = header.os_version&0x7ff;
+
+            a = (os_version >> 14)&0x7f;
+            b = (os_version >> 7)&0x7f;
+            c = os_version&0x7f;
+
+            y = (os_patch_level >> 4) + 2000;
+            m = os_patch_level&0xf;
+
+            printf("  magic                 : ANDROID!\n");
+            printf("  kernel_size           : %-10d  (%08x)\n", header.kernel_size, header.kernel_size);
+            printf("  ramdisk_size          : %-10d  (%08x)\n\n", header.ramdisk_size, header.ramdisk_size);
+
+            if ((a < 128) && (b < 128) && (c < 128) && (y >= 2000) && (y < 2128) && (m > 0) && (m <= 12)) {
+                printf("  os_version            : %d.%d.%d\n", a, b, c);
+                printf("  os_patch_level        : %d-%02d\n", y, m);
+            }
+            printf("  header_size           : %-10d  (%08x)\n", header.header_size, header.header_size);
+            printf("  reserved[1]           : %-10d  (%08x)\n", header.reserved[0], header.reserved[0]);
+            printf("  reserved[2]           : %-10d  (%08x)\n\n", header.reserved[1], header.reserved[1]);
+
+            printf("  reserved[3]           : %-10d  (%08x)\n", header.reserved[2], header.reserved[2]);
+            printf("  reserved[4]           : %-10d  (%08x)\n", header.reserved[3], header.reserved[3]);
+            printf("  header_version        : %-10d  (%08x)\n", header.header_version, header.header_version);
+            printf("  cmdline               : %.*s\n\n", BOOT_ARGS_SIZE+BOOT_EXTRA_ARGS_SIZE, header.cmdline);
+
+            printf(" Other:\n");
+            printf("  magic offset          : 0x%08x\n", i);
+        }
     } else {
+        // vendor_boot_img_hdr started at v3 and is not cross-compatible with boot_img_hdr
+        fseek(f, i, SEEK_SET);
+        vendor_boot_img_hdr_v3 header;
+        if(fread(&header, sizeof(header), 1, f)){};
+
+        base = header.kernel_addr - 0x00008000;
+
+        printf("  magic                 : VNDRBOOT\n");
         printf("  header_version        : %-10d  (%08x)\n", header.header_version, header.header_version);
-    }
-    if ((a < 128) && (b < 128) && (c < 128) && (y >= 2000) && (y < 2128) && (m > 0) && (m <= 12)) {
-        printf("  os_version            : %d.%d.%d\n", a, b, c);
-        printf("  os_patch_level        : %d-%02d\n\n", y, m);
-    } else {
-        printf("  unused                : %-10d  (%08x)\n\n", header.os_version, header.os_version);
-    }
+        printf("  page_size             : %-10d  (%08x)\n\n", header.page_size, header.page_size);
 
-    printf("  name                  : %s\n\n", header.name);
+        printf("  kernel_addr           : 0x%08x\n", header.kernel_addr);
+        printf("  ramdisk_addr          : 0x%08x\n", header.ramdisk_addr);
+        printf("  vendor_ramdisk_size   : %-10d  (%08x)\n", header.vendor_ramdisk_size, header.vendor_ramdisk_size);
+        printf("  cmdline               : %.*s\n", VENDOR_BOOT_ARGS_SIZE, header.cmdline);
+        printf("  tags_addr             : 0x%08x\n\n", header.tags_addr);
 
-    printf("  cmdline               : %.*s\n\n", BOOT_ARGS_SIZE, header.cmdline);
+        printf("  name                  : %s\n\n", header.name);
 
-    printf("  id                    : "); print_hash(&header); printf("\n\n");
+        printf("  header_size           : %-10d  (%08x)\n", header.header_size, header.header_size);
+        printf("  dtb_size              : %-10d  (%08x)\n", header.dtb_size, header.dtb_size);
+        printf("  dtb_addr              : 0x%08"PRIx64"  (%016"PRIx64")\n\n", header.dtb_addr, header.dtb_addr);
 
-    printf("  extra_cmdline         : %.*s\n\n", BOOT_EXTRA_ARGS_SIZE, header.extra_cmdline);
+        printf(" Other:\n");
+        printf("  magic offset          : 0x%08x\n", i);
+        printf("  base address          : 0x%08x\n\n", base);
 
-    if (header.header_version <= hdr_ver_max) {
-        if (header.header_version > 0) {
-            printf("  recovery_dtbo_size    : %-10d  (%08x)\n", header.recovery_dtbo_size, header.recovery_dtbo_size);
-            printf("  recovery_dtbo_offset  : %-10"PRId64"  (%016"PRIx64")\n", header.recovery_dtbo_offset, header.recovery_dtbo_offset);
-            printf("  header_size           : %-10d  (%08x)\n\n", header.header_size, header.header_size);
-        }
-        if (header.header_version > 1) {
-            printf("  dtb_size              : %-10d  (%08x)\n", header.dtb_size, header.dtb_size);
-            printf("  dtb_addr              : 0x%08"PRIx64"  (%016"PRIx64")\n\n", header.dtb_addr, header.dtb_addr);
-        }
-    }
-
-    printf(" Other:\n");
-    printf("  magic offset          : 0x%08x\n", i);
-    printf("  base address          : 0x%08x\n\n", base);
-
-    printf("  kernel offset         : 0x%08x\n", header.kernel_addr - base);
-    printf("  ramdisk offset        : 0x%08x\n", header.ramdisk_addr - base);
-    printf("  second offset         : 0x%08x\n", header.second_addr - base);
-    printf("  tags offset           : 0x%08x\n", header.tags_addr - base);
-    if (header.header_version <= hdr_ver_max && header.header_version > 1) {
+        printf("  kernel offset         : 0x%08x\n", header.kernel_addr - base);
+        printf("  ramdisk offset        : 0x%08x\n", header.ramdisk_addr - base);
+        printf("  tags offset           : 0x%08x\n", header.tags_addr - base);
         printf("  dtb offset            : 0x%08"PRIx64"\n", header.dtb_addr - base);
     }
 
--- bootimg.h
+++ bootimg.h
@@ -24,49 +24,10 @@
 #define BOOT_ARGS_SIZE 512
 #define BOOT_EXTRA_ARGS_SIZE 1024
 
-#define BOOT_HEADER_VERSION_ZERO 0
-
-// The bootloader expects the structure of boot_img_hdr with header
-// version 0 to be as follows:
-struct boot_img_hdr_v0 {
-    // Must be BOOT_MAGIC.
-    uint8_t magic[BOOT_MAGIC_SIZE];
-
-    uint32_t kernel_size; /* size in bytes */
-    uint32_t kernel_addr; /* physical load addr */
-
-    uint32_t ramdisk_size; /* size in bytes */
-    uint32_t ramdisk_addr; /* physical load addr */
-
-    uint32_t second_size; /* size in bytes */
-    uint32_t second_addr; /* physical load addr */
-
-    uint32_t tags_addr; /* physical addr for kernel tags */
-    uint32_t page_size; /* flash page size we assume */
-
-    // Version of the boot image header.
-    // Alternately this is used as dt_size on some hardware.
-    union {
-        uint32_t header_version;
-        uint32_t dt_size; /* device tree in bytes */
-    };
-
-    // Operating system version and security patch level.
-    // For version "A.B.C" and patch level "Y-M-D":
-    //   (7 bits for each of A, B, C; 7 bits for (Y-2000), 4 bits for M)
-    //   os_version = A[31:25] B[24:18] C[17:11] (Y-2000)[10:4] M[3:0]
-    uint32_t os_version;
-
-    uint8_t name[BOOT_NAME_SIZE]; /* asciiz product name */
-
-    uint8_t cmdline[BOOT_ARGS_SIZE];
-
-    uint32_t id[8]; /* timestamp / checksum / sha1 / etc */
-
-    // Supplemental command line data; kept here to maintain
-    // binary compatibility with older versions of mkbootimg.
-    uint8_t extra_cmdline[BOOT_EXTRA_ARGS_SIZE];
-} __attribute__((packed));
+#define VENDOR_BOOT_MAGIC "VNDRBOOT"
+#define VENDOR_BOOT_MAGIC_SIZE 8
+#define VENDOR_BOOT_ARGS_SIZE 2048
+#define VENDOR_BOOT_NAME_SIZE 16
 
 /*
  * It is expected that callers would explicitly specify which version of the
@@ -76,6 +37,8 @@ typedef struct boot_img_hdr_v0 boot_img_hdr;
 typedef struct boot_img_hdr_v0 boot_img_hdr_v0;
 typedef struct boot_img_hdr_v1 boot_img_hdr_v1;
 typedef struct boot_img_hdr_v2 boot_img_hdr_v2;
+typedef struct boot_img_hdr_v3 boot_img_hdr_v3;
+typedef struct vendor_boot_img_hdr_v3 vendor_boot_img_hdr_v3;
 
 /* When a boot header is of version 0, the structure of boot image is as
  * follows:
@@ -113,10 +76,7 @@ typedef struct boot_img_hdr_v2 boot_img_hdr_v2;
  * 6. if second_size != 0: jump to second_addr
  *    else: jump to kernel_addr
  */
-
-#define BOOT_HEADER_VERSION_ONE 1
-
-struct boot_img_hdr_v1 {
+struct boot_img_hdr_v0 {
     // Must be BOOT_MAGIC.
     uint8_t magic[BOOT_MAGIC_SIZE];
 
@@ -129,7 +89,7 @@ struct boot_img_hdr_v1 {
     uint32_t second_size; /* size in bytes */
     uint32_t second_addr; /* physical load addr */
 
-    uint32_t tags_addr; /* physical addr for kernel tags */
+    uint32_t tags_addr; /* physical addr for kernel tags (if required) */
     uint32_t page_size; /* flash page size we assume */
 
     // Version of the boot image header.
@@ -147,17 +107,14 @@ struct boot_img_hdr_v1 {
 
     uint8_t name[BOOT_NAME_SIZE]; /* asciiz product name */
 
-    uint8_t cmdline[BOOT_ARGS_SIZE];
+    uint8_t cmdline[BOOT_ARGS_SIZE]; /* asciiz kernel commandline */
 
     uint32_t id[8]; /* timestamp / checksum / sha1 / etc */
 
     // Supplemental command line data; kept here to maintain
     // binary compatibility with older versions of mkbootimg.
+    // Asciiz.
     uint8_t extra_cmdline[BOOT_EXTRA_ARGS_SIZE];
-
-    uint32_t recovery_dtbo_size;   /* size in bytes for recovery DTBO/ACPIO image */
-    uint64_t recovery_dtbo_offset; /* offset to recovery dtbo/acpio in boot image */
-    uint32_t header_size;
 } __attribute__((packed));
 
 /* When the boot image header has a version of 1, the structure of the boot
@@ -190,16 +147,11 @@ struct boot_img_hdr_v1 {
  * 5. If booting to recovery mode in a non-A/B device, extract recovery
  *    dtbo/acpio and apply the correct set of overlays on the base device tree
  *    depending on the hardware/product revision.
- * 6. prepare tags at tag_addr.  kernel_args[] is
- *    appended to the kernel commandline in the tags.
- * 7. r0 = 0, r1 = MACHINE_TYPE, r2 = tags_addr
- * 8. if second_size != 0: jump to second_addr
+ * 6. set up registers for kernel entry as required by your architecture
+ * 7. if second_size != 0: jump to second_addr
  *    else: jump to kernel_addr
  */
-
-#define BOOT_HEADER_VERSION_TWO 2
-
-struct boot_img_hdr_v2 {
+struct boot_img_hdr_v1 {
     // Must be BOOT_MAGIC.
     uint8_t magic[BOOT_MAGIC_SIZE];
 
@@ -212,11 +164,12 @@ struct boot_img_hdr_v2 {
     uint32_t second_size; /* size in bytes */
     uint32_t second_addr; /* physical load addr */
 
-    uint32_t tags_addr; /* physical addr for kernel tags */
+    uint32_t tags_addr; /* physical addr for kernel tags (if required) */
     uint32_t page_size; /* flash page size we assume */
 
     // Version of the boot image header.
     // Alternately this is used as dt_size on some hardware.
+    // Kept here for backwards compatibility.
     union {
         uint32_t header_version;
         uint32_t dt_size; /* device tree in bytes */
@@ -230,20 +183,18 @@ struct boot_img_hdr_v2 {
 
     uint8_t name[BOOT_NAME_SIZE]; /* asciiz product name */
 
-    uint8_t cmdline[BOOT_ARGS_SIZE];
+    uint8_t cmdline[BOOT_ARGS_SIZE]; /* asciiz kernel commandline */
 
     uint32_t id[8]; /* timestamp / checksum / sha1 / etc */
 
     // Supplemental command line data; kept here to maintain
     // binary compatibility with older versions of mkbootimg.
+    // Asciiz.
     uint8_t extra_cmdline[BOOT_EXTRA_ARGS_SIZE];
 
     uint32_t recovery_dtbo_size;   /* size in bytes for recovery DTBO/ACPIO image */
     uint64_t recovery_dtbo_offset; /* offset to recovery dtbo/acpio in boot image */
     uint32_t header_size;
-
-    uint32_t dtb_size; /* size in bytes for DTB image */
-    uint64_t dtb_addr; /* physical load address for DTB image */
 } __attribute__((packed));
 
 /* When the boot image header has a version of 2, the structure of the boot
@@ -279,9 +230,149 @@ struct boot_img_hdr_v2 {
  * 5. If booting to recovery mode in a non-A/B device, extract recovery
  *    dtbo/acpio and apply the correct set of overlays on the base device tree
  *    depending on the hardware/product revision.
- * 6. prepare tags at tag_addr.  kernel_args[] is
- *    appended to the kernel commandline in the tags.
- * 7. r0 = 0, r1 = MACHINE_TYPE, r2 = tags_addr
- * 8. if second_size != 0: jump to second_addr
+ * 6. set up registers for kernel entry as required by your architecture
+ * 7. if second_size != 0: jump to second_addr
  *    else: jump to kernel_addr
  */
+struct boot_img_hdr_v2 {
+    // Must be BOOT_MAGIC.
+    uint8_t magic[BOOT_MAGIC_SIZE];
+
+    uint32_t kernel_size; /* size in bytes */
+    uint32_t kernel_addr; /* physical load addr */
+
+    uint32_t ramdisk_size; /* size in bytes */
+    uint32_t ramdisk_addr; /* physical load addr */
+
+    uint32_t second_size; /* size in bytes */
+    uint32_t second_addr; /* physical load addr */
+
+    uint32_t tags_addr; /* physical addr for kernel tags (if required) */
+    uint32_t page_size; /* flash page size we assume */
+
+    // Version of the boot image header.
+    // Alternately this is used as dt_size on some hardware.
+    // Kept here for backwards compatibility.
+    union {
+        uint32_t header_version;
+        uint32_t dt_size; /* device tree in bytes */
+    };
+
+    // Operating system version and security patch level.
+    // For version "A.B.C" and patch level "Y-M-D":
+    //   (7 bits for each of A, B, C; 7 bits for (Y-2000), 4 bits for M)
+    //   os_version = A[31:25] B[24:18] C[17:11] (Y-2000)[10:4] M[3:0]
+    uint32_t os_version;
+
+    uint8_t name[BOOT_NAME_SIZE]; /* asciiz product name */
+
+    uint8_t cmdline[BOOT_ARGS_SIZE]; /* asciiz kernel commandline */
+
+    uint32_t id[8]; /* timestamp / checksum / sha1 / etc */
+
+    // Supplemental command line data; kept here to maintain
+    // binary compatibility with older versions of mkbootimg.
+    // Asciiz.
+    uint8_t extra_cmdline[BOOT_EXTRA_ARGS_SIZE];
+
+    uint32_t recovery_dtbo_size;   /* size in bytes for recovery DTBO/ACPIO image */
+    uint64_t recovery_dtbo_offset; /* offset to recovery dtbo/acpio in boot image */
+    uint32_t header_size;
+
+    uint32_t dtb_size; /* size in bytes for DTB image */
+    uint64_t dtb_addr; /* physical load address for DTB image */
+} __attribute__((packed));
+
+/* When the boot image header has a version of 3, the structure of the boot
+ * image is as follows:
+ *
+ * +---------------------+
+ * | boot header         | 4096 bytes
+ * +---------------------+
+ * | kernel              | m pages
+ * +---------------------+
+ * | ramdisk             | n pages
+ * +---------------------+
+ *
+ * m = (kernel_size + 4096 - 1) / 4096
+ * n = (ramdisk_size + 4096 - 1) / 4096
+ *
+ * Note that in version 3 of the boot image header, page size is fixed at 4096 bytes.
+ *
+ * The structure of the vendor boot image (introduced with version 3 and
+ * required to be present when a v3 boot image is used) is as follows:
+ *
+ * +---------------------+
+ * | vendor boot header  | o pages
+ * +---------------------+
+ * | vendor ramdisk      | p pages
+ * +---------------------+
+ * | dtb                 | q pages
+ * +---------------------+
+
+ * o = (2112 + page_size - 1) / page_size
+ * p = (vendor_ramdisk_size + page_size - 1) / page_size
+ * q = (dtb_size + page_size - 1) / page_size
+ *
+ * 0. all entities in the boot image are 4096-byte aligned in flash, all
+ *    entities in the vendor boot image are page_size (determined by the vendor
+ *    and specified in the vendor boot image header) aligned in flash
+ * 1. kernel, ramdisk, vendor ramdisk, and DTB are required (size != 0)
+ * 2. load the kernel and DTB at the specified physical address (kernel_addr,
+ *    dtb_addr)
+ * 3. load the vendor ramdisk at ramdisk_addr
+ * 4. load the generic ramdisk immediately following the vendor ramdisk in
+ *    memory
+ * 5. set up registers for kernel entry as required by your architecture
+ * 6. if the platform has a second stage bootloader jump to it (must be
+ *    contained outside boot and vendor boot partitions), otherwise
+ *    jump to kernel_addr
+ */
+struct boot_img_hdr_v3 {
+    // Must be BOOT_MAGIC.
+    uint8_t magic[BOOT_MAGIC_SIZE];
+
+    uint32_t kernel_size; /* size in bytes */
+    uint32_t ramdisk_size; /* size in bytes */
+
+    // Operating system version and security patch level.
+    // For version "A.B.C" and patch level "Y-M-D":
+    //   (7 bits for each of A, B, C; 7 bits for (Y-2000), 4 bits for M)
+    //   os_version = A[31:25] B[24:18] C[17:11] (Y-2000)[10:4] M[3:0]
+    uint32_t os_version;
+
+    uint32_t header_size;
+
+    uint32_t reserved[4];
+
+    // Version of the boot image header.
+    uint32_t header_version;
+
+    // Asciiz kernel commandline.
+    uint8_t cmdline[BOOT_ARGS_SIZE + BOOT_EXTRA_ARGS_SIZE];
+} __attribute__((packed));
+
+struct vendor_boot_img_hdr_v3 {
+    // Must be VENDOR_BOOT_MAGIC.
+    uint8_t magic[VENDOR_BOOT_MAGIC_SIZE];
+
+    // Version of the vendor boot image header.
+    uint32_t header_version;
+
+    uint32_t page_size; /* flash page size we assume */
+
+    uint32_t kernel_addr; /* physical load addr */
+    uint32_t ramdisk_addr; /* physical load addr */
+
+    uint32_t vendor_ramdisk_size; /* size in bytes */
+
+    uint8_t cmdline[VENDOR_BOOT_ARGS_SIZE]; /* asciiz kernel commandline */
+
+    uint32_t tags_addr; /* physical addr for kernel tags (if required) */
+    uint8_t name[VENDOR_BOOT_NAME_SIZE]; /* asciiz product name */
+
+    uint32_t header_size;
+
+    uint32_t dtb_size; /* size in bytes for DTB image */
+    uint64_t dtb_addr; /* physical load address for DTB image */
+} __attribute__((packed));
